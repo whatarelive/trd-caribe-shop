4955aeef8271c5dbe68b902e64cfdc538da3a63d
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-03-09 20:16:08 +0100

    Mejore la página de productos de administración con diseño responsivo y vistas de tabla/lista
    - Se agregó entrada de búsqueda y botón "Nuevo producto" a la página de productos.
    - Diseño responsivo implementado con vistas de escritorio y móviles.
    - Se crearon los componentes ProductsTable y ProductsList.
    - Se agregó un marcador de posición para la tabla de productos con marcadores de posición de imagen y detalles.

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,13 +1,38 @@
+import Link from "next/link";
+import { IoAddOutline } from "react-icons/io5";
 import { getProducts } from "@/src/lib/actions/products";
-
+import { ProductsTable } from "@/src/components/admin/products/products-table";
+import { ProductsList } from "@/src/components/admin/products/products-list";
+import { SearchInput } from "@/src/components/ui/input/input-search";
 export default async function ProductsPage() {
     const products = await getProducts();
 
     console.log(products);
 
     return (
-        <div>
-            <h1>Productos</h1>
-        </div>
+        <section className="flex flex-col gap-4 p-8">
+            <div className="flex flex-col min-[500px]:flex-row gap-4 justify-between min-[500px]:items-center">
+                <h1 className="text-2xl lg:text-3xl font-semibold text-neutral-500">
+                    Productos
+                </h1>
+
+                <div className="flex items-center gap-2 lg:gap-4">
+                    <SearchInput />
+
+                    <Link href="/admin/products/create" className="inline-flex items-center gap-2 font-normal p-2 lg:p-3 rounded-md bg-blue-500 text-white hover:bg-blue-600">
+                        <IoAddOutline size={24} />
+                        <span className="hidden lg:block">Nuevo</span>
+                    </Link>
+                </div>
+            </div>
+            
+            <div className="hidden lg:block p-4 bg-white rounded-lg mt-6">
+                <ProductsTable />
+            </div>
+
+            <div className="lg:hidden">
+                <ProductsList />
+            </div>
+        </section>
     );
 }
--- src/components/admin/products/products-table.tsx
+++ src/components/admin/products/products-table.tsx
@@ -1,6 +1,85 @@
+import Image from "next/image"
+import { Table, TableHeader, TableHead, TableRow, TableBody, TableCell } from "@/src/components/ui/table"
 
 export const ProductsTable = () => {
     return (
-        <div>ProductsTable</div>
+        <Table className="min-h-[700px]">
+            <TableHeader>
+                <TableRow className="border-neutral-300 border-b">
+                    <TableHead>
+                        Producto
+                    </TableHead>
+                    <TableHead>
+                        Precio
+                    </TableHead>
+                    <TableHead>
+                        Descuento
+                    </TableHead>
+                    <TableHead>
+                        Categoría
+                    </TableHead>
+                    <TableHead>
+                        Disponibilidad
+                    </TableHead>
+                    <TableHead>
+                        Opciones
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                { [].map((product, index) => (
+                    <TableRow key={index} className="hover:bg-p_gray_100 hover:rounded-lg">
+                        <TableCell className="flex gap-4">
+                            <Image 
+                                src={""} 
+                                alt={""}
+                                width={64}
+                                height={64}
+                                loading="lazy"
+                                className="rounded-2xl w-16 h-16"
+                            />
+        
+                            <div className="flex flex-col justify-center">
+                                {/* <h2 
+                                    className={clsx(
+                                        'font-medium',
+                                        { 
+                                        'text-p_green' : product.stock,
+                                        'text-p_rose_900' : !product.stock,
+                                        }
+                                    )}
+                                    >
+                                        {product.title}
+                                </h2>
+        
+                                <p className="text-wrap text-[13px] line-clamp-2 max-w-[266px] text-p_gray_600">
+                                    {product.subtitle}
+                                </p> */}
+                            </div>
+                        </TableCell>
+                            
+                        <TableCell className="text-p_gray_900 font-medium">
+                            {/* $ { 
+                                product.price.toString().length === 3 
+                                    ? product.price.toPrecision(5)
+                                    : product.price.toPrecision(6)
+                            } */}
+                        </TableCell>
+                            
+                        <TableCell>
+                            {/* <ProductCategory category={product.category}/> */}
+                        </TableCell>
+                        
+                        <TableCell>
+                            {/* <ProductDisponibility stock={product.stock}/> */}
+                        </TableCell>
+                        
+                        <TableCell>
+                            {/* <ProductOptions productId={product.id} name={product.title}/> */}
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     )
 }
