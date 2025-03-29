0af8253ac466566b127e215cd5721416d2fd67b4
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-03-29 22:17:48 +0100

    Se agrego el componente de filtros en la página de ventas

--- src/app/admin/sales/page.tsx
+++ src/app/admin/sales/page.tsx
@@ -3,8 +3,15 @@ import { TitlePage } from "@/components/admin/title-page";
 import { BreadcrumbsPage } from "@/components/admin/breadcrumbs-page";
 import { ToolsSectionPage } from "@/components/admin/tools-section-page";
 import { SalesTable } from "@/components/admin/sales/sales-table";
+import { SaleStateFilters } from "@/components/admin/sales/modal-state-filters";
 
-export default function SalesPage() {
+interface Props {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export default async function SalesPage({ searchParams }: Props) {
+    const { page, status } = await searchParams;
+
     return (
         <section className="flex flex-col gap-6 w-full p-4 min-[375px]:p-8 xl:pr-16">
             <div>
@@ -19,6 +26,8 @@ export default function SalesPage() {
             </div>
 
             <ToolsSectionPage placeholder="Buscar ventas"/>
+
+            <SaleStateFilters filters={status}/>
             
             <Suspense fallback={<p>Cargando...</p>}>
                 <SalesTable/>
